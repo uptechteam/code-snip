092c2921a4f18571d7a0848ff655883c72f615a4
arthur-here <arthur.myronenko@gmail.com>
2020-10-02 17:01:50 +0300

    Updates

---
+++ precache-manifest.e0fdfbb1461d59beaaa8c05c965f1f7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4968898ec5e1d7bcfb3f57c03bd68a",
+    "url": "/code-snip/index.html"
+  },
+  {
+    "revision": "27550693673c9e79f068",
+    "url": "/code-snip/static/css/main.eb8475b1.chunk.css"
+  },
+  {
+    "revision": "f3a6bd86dabda2bf19ec",
+    "url": "/code-snip/static/js/2.345e258a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/code-snip/static/js/2.345e258a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27550693673c9e79f068",
+    "url": "/code-snip/static/js/main.3d6aca11.chunk.js"
+  },
+  {
+    "revision": "b760e8448329c2685770",
+    "url": "/code-snip/static/js/runtime-main.71635adc.js"
+  },
+  {
+    "revision": "0c315dc9493dc13dfb8f78e6bb0dc887",
+    "url": "/code-snip/static/media/uptech_logo.0c315dc9.svg"
+  }
+]);
